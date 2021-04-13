92fc67ae2bf89b0365cbcf88861c361d197b5d61
majingran <815212447@qq.com>
2021-04-13 23:07:02 +0800

    Spring -----------add-----------Lambda---------

--- src/main/java/com/example/Lambda.java
+++ src/main/java/com/example/Lambda.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lambda {
     public static void main(String[] args) {
         new Thread(() -> System.out.println("Lambda rocks !！")).start();
+        List features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        features.forEach(System.out::println);
     }
-
 }
