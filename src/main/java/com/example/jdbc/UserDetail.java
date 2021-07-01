9839e3a92e6050fa6d4525b5c0f3647a4f7a17c1
majingran <815212447@qq.com>
2021-07-02 00:13:06 +0800

    -----整合spring security ---------------

--- src/main/java/com/example/jdbc/UserDetail.java
+++ src/main/java/com/example/jdbc/UserDetail.java
@@ -1,10 +1,18 @@
 package com.example.jdbc;
 
+import com.example.jdbc.model.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
 
 public class UserDetail extends User {
 
     private UserEntity userEntity;
 
-    public UserDetail()
+    public UserDetail (UserEntity userEntity, Collection<? extends GrantedAuthority> authorities){
+        // 必须调用父类的构造方法，初始化用户名、密码、权限
+        super(userEntity.getUsername(), userEntity.getPassword(), authorities);
+        this.userEntity = userEntity;
+    }
 }
---
+++ src/main/java/com/example/jdbc/model/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package com.example.jdbc.model.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName("user")
+public class UserEntity extends BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 用户密码
+     */
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
