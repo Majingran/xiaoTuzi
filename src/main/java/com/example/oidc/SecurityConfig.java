ae5659d1254b7f53dc99a775401741034ff72c35
majingran <815212447@qq.com>
2021-07-15 00:26:36 +0800

    -----整合spring security ---------------

--- src/main/java/com/example/oidc/SecurityConfig.java
+++ src/main/java/com/example/oidc/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.oidc;
 
+import com.example.jdbc.LoginFilter;
+import com.example.oidc.handle.AuthenticationEntryPointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,40 +11,62 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    /**
+     * @Description: 自定义用户逻辑
+     **/
     @Autowired
     private UserDetailsService userService;
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
-//                .withUser("admin").password(passwordEncoder().encode("123456")).authorities("ADMIN")
-//                .and()
-//                .withUser("lisi").password(passwordEncoder().encode("123456")).authorities("ORDINARY");
-//    }
+    /**
+     * @Description: 认证失败处理类
+     **/
+    @Autowired
+    private AuthenticationEntryPointImpl unauthorizeHandler;
 
+    @Autowired
+    private LoginFilter loginFilter;
 
+    /**
+     * anyRequest          |   匹配所有请求路径
+     * access              |   SpringEl表达式结果为true时可以访问
+     * anonymous           |   匿名可以访问
+     * denyAll             |   用户不能访问
+     * fullyAuthenticated  |   用户完全认证可以访问（非remember-me下自动登录）
+     * hasAnyAuthority     |   如果有参数，参数表示权限，则其中任何一个权限可以访问
+     * hasAnyRole          |   如果有参数，参数表示角色，则其中任何一个角色可以访问
+     * hasAuthority        |   如果有参数，参数表示权限，则其权限可以访问
+     * hasIpAddress        |   如果有参数，参数表示IP地址，如果用户IP和参数匹配，则可以访问
+     * hasRole             |   如果有参数，参数表示角色，则其角色可以访问
+     * permitAll           |   用户可以任意访问
+     * rememberMe          |   允许通过remember-me登录的用户访问
+     * authenticated       |   用户登录后可访问
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/","/**","/**","/**").permitAll()
-//                .antMatchers("/**")
-//                .hasRole("ADMIN");
         //关闭csrf
         http.csrf().disable();
         http.headers().frameOptions().disable();
         //允许跨域访问
         http.cors().and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .antMatchers("com/rabbit/user/logout").permitAll();
+//                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/*/*/*/login").anonymous()
+                .antMatchers("com/rabbit/user/logout").permitAll()
+                .anyRequest().authenticated();
+        http.exceptionHandling().authenticationEntryPoint(unauthorizeHandler);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
--- src/main/java/com/example/oidc/handle/AuthenticationEntryPointImpl.java
+++ src/main/java/com/example/oidc/handle/AuthenticationEntryPointImpl.java
@@ -7,6 +7,7 @@ import com.example.common.utils.ServletUtils;
 import com.example.common.utils.StringUtils;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serializable;
 
+/**
+ * @Description: 认证失败处理类
+ * @Author: Mr_ma
+ * @Date: 2021-7-14 23:25
+ * @return: 未授权
+ **/
+@Component
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint, Serializable {
 
 
